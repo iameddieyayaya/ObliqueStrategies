6372466f8a4265b3c13d91d784d961d7442d86e3
Eddie Gomez <eddieegomez4@gmail.com>
2020-02-22 17:54:53 -0800

    card and background up

--- src/App.js
+++ src/App.js
@@ -1,14 +1,65 @@
-import React, { useEffect } from 'react';
+import React, { useState, useEffect } from 'react';
+import { useSpring, animated } from 'react-spring';
 import './App.css';
 
+// import { ACCESS_KEY, SECRET_KEY } from './keys';
+
 function App() {
+  const [background, setBackground] = useState();
+  const [strategy, setStrategy] = useState();
+
+  const props = useSpring({
+    to: async (next, cancel) => {
+      await next({ opacity: 0, color: 'rgb(14,26,19)' });
+      await next({ opacity: 1, color: '#000' });
+    },
+    from: { opacity: 0, color: 'red' }
+  });
+
+  const style = {
+    backgroundImage: `url(${background})`,
+    backgroundRepeat: 'no-repeat',
+    backgroundSize: 'cover',
+    display: 'flex',
+    justifyContent: 'center',
+    alignItems: 'center'
+  };
+
   useEffect(() => {
     fetch('https://oblique-stragies-01.herokuapp.com/api/random')
       .then(response => response.json())
-      .then(json => console.log(json));
-  });
+      .then(data => setStrategy(data.randomStrategy));
 
-  return <div className='App'>hey</div>;
+    // fetch(`https://api.unsplash.com/photos/random/?client_id=${ACCESS_KEY}`)
+    //   .then(response => response.json())
+    //   .then(data => setBackground(data.urls.full));
+
+    fetch(`https://source.unsplash.com/featured/?texture,patterns`)
+      .then(response => response)
+      .then(data => setBackground(data.url));
+  }, []);
+
+  return (
+    <div style={style} className='App'>
+      <animated.div style={props}>
+        <div
+          style={{
+            display: 'flex',
+            justifyContent: 'center',
+            alignItems: 'center',
+            width: '415px',
+            height: '220px',
+            textAlign: 'center',
+            padding: '10px',
+            margin: '10px',
+            background: '#FFF',
+            borderRadius: '5%'
+          }}>
+          <h3>{strategy}</h3>
+        </div>
+      </animated.div>
+    </div>
+  );
 }
 
 export default App;
